f4199c83d4f4101ccfe9d73dcbe72ebaf6c51aea
Nalofree <nalofree@gmail.com>
2016-03-18 13:43:17 +0800

    favicon added 3

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ arMap.use(function(req,res,next){
 });
 
 //arMap.set('views', path.join(__dirname, 'views/'));
-arMap.use(express.static(__dirname + '/views/'));
+arMap.use(express.static(__dirname + 'views/'));
 arMap.use(bodyParser.urlencoded({extended: true}));
 
 arMap.get('/auth', function(req,res){
