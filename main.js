7cb66ef6a42e1002adc3a57cd52fd6db3d486d7c
Nalofree <nalofree@gmail.com>
2016-03-18 22:38:21 +0800

    new 36

--- main.js
+++ main.js
@@ -34,10 +34,9 @@ arMap.use(function(req,res,next){
 	next();
 });
 
-arMap.set('public', path.join(__dirname, '/public/'));
+arMap.set('views', path.join(__dirname, '/views'));
 //console.log(__dirname);
-//arMap.use(express.static(__dirname+'/public'));
-arMap.use(express.static('public'));
+arMap.use(express.static(__dirname+'/views'));
 arMap.use(bodyParser.urlencoded({extended: true}));
 
 arMap.get('/auth', function(req,res){
